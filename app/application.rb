26bbcae63f4f674dd38950895b2428a89223c3f8
Jonathan Norman <jnorman2123@gmail.com>
2019-11-14 00:15:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application
   def call(env)
-    resp = Rack::Response.new\
+    resp = Rack::Response.new
     morning_greeting = resp.write "Good Morning!"
     afternoon_greeting = resp.write "Good Afternoon!"
     Time.hour < 12 ? morning_greeting : afternoon_greeting
