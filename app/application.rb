7ab1c582ce4cac49867fe1f8f52dbd2727742fdc
Jonathan Norman <jnorman2123@gmail.com>
2019-11-14 00:18:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,8 @@ class Application
     resp = Rack::Response.new
     morning_greeting = resp.write "Good Morning!"
     afternoon_greeting = resp.write "Good Afternoon!"
-    Time.hour < 12 ? morning_greeting : afternoon_greeting
+    t = Time.now
+    t.hour < 12 ? morning_greeting : afternoon_greeting
 
     resp.finish
   end
